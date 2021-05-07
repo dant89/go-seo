7025b1a60afba8661ae0a468538c71cf71262bc5
Dan Thorpe <dant@linx.net>
2021-05-07 15:33:28 +0100

    Replace analyser public struct values with public interface functions

--- analyser.go
+++ analyser.go
@@ -1,25 +1,42 @@
 package goseo
 
 type Report struct {
-	Passed   bool
-	Feedback []string
+	passed   bool
+	feedback []string
+}
+
+type Reporter interface {
+	AddFeedback()
+	GetFeedback()
 }
 
 const H1ShortError = "The H1 is too short, aim for 20 characters minimum."
 const H1LongError = "The H1 is too long, aim for 70 characters maximum."
 
 func CheckH1Length(h1 string) Report {
-	report := Report{Passed: true}
+	report := Report{passed: true}
 
 	if len(h1) < 20 {
-		report.Passed = false
-		report.Feedback = append(report.Feedback, H1ShortError)
+		report.passed = false
+		report.AddFeedback(H1ShortError)
 	}
 
 	if len(h1) > 70 {
-		report.Passed = false
-		report.Feedback = append(report.Feedback, H1LongError)
+		report.passed = false
+		report.AddFeedback(H1LongError)
 	}
 
 	return report
+}
+
+func (r Report) Passed() bool {
+	return r.passed
+}
+
+func (r Report) AddFeedback(feedback string) {
+	r.feedback = append(r.feedback, feedback)
+}
+
+func (r Report) GetFeedback() []string {
+	return r.feedback
 }
--- analyser_test.go
+++ analyser_test.go
@@ -9,8 +9,8 @@ import (
 func TestAnalyser(t *testing.T) {
 	h1Short := "This is a test"
 	h1ShortResult := goseo.CheckH1Length(h1Short)
-	if h1ShortResult.Passed == false {
-		for _, err := range h1ShortResult.Feedback {
+	if !h1ShortResult.Passed() {
+		for _, err := range h1ShortResult.GetFeedback() {
 			if err != goseo.H1ShortError {
 				t.Errorf("Incorrect message for short H1")
 			}
@@ -19,8 +19,8 @@ func TestAnalyser(t *testing.T) {
 
 	h1Long := "This is a test, This is a test, This is a test, This is a test, This is a test, This is a test"
 	h1LongResult := goseo.CheckH1Length(h1Long)
-	if h1LongResult.Passed == false {
-		for _, err := range h1LongResult.Feedback {
+	if !h1LongResult.Passed() {
+		for _, err := range h1LongResult.GetFeedback() {
 			if err != goseo.H1LongError {
 				t.Errorf("Incorrect message for long H1")
 			}
@@ -29,8 +29,8 @@ func TestAnalyser(t *testing.T) {
 
 	h1Perfect := "This is a test, This is a test, This is a test"
 	h1PerfectResult := goseo.CheckH1Length(h1Perfect)
-	if h1PerfectResult.Passed == true {
-		if len(h1PerfectResult.Feedback) > 0 {
+	if !h1PerfectResult.Passed() {
+		if len(h1PerfectResult.GetFeedback()) > 0 {
 			t.Errorf("Incorrect message for perfect H1.")
 		}
 	}
